63f4e038dc65141505a79da6248a8b5909d6e9bc
Kawesom <Kamaladeyinka@yahoo.com>
2024-03-22 03:30:56 +1100

    Ep19- Composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import { ref } from 'vue';
+
+let message = ref('Dogtooth');
+
+  setTimeout(() =>{
+     message.value = "Howdy";
+  }, 2000);
+
+let doSomething = () => {
+  alert("Do It");
+}
+
+
 </script>
 
 <template>
   <main>
- 
+ <p>{{ message }}</p>
+ <p>
+  <button @click="doSomething">Clicky Blicky</button>
+ </p>
     <TheWelcome />
   </main>
 </template>
