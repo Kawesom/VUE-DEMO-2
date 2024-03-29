c40002f983452e8c479eeda57a8ea6259203503a
Kawesom <Kamaladeyinka@yahoo.com>
2024-03-30 10:28:54 +1100

    Ep-25 Intro to Pinia

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,13 +1,17 @@
 <script setup>
-import {counter} from "@/stores/counterStore.js";
+import { useCounterStore } from "@/stores/counterStore.js";
+
+let counter = useCounterStore();
 </script>
 
 <template>
-  <div class="about">
+  <div>
     <h1>
       {{ counter.count }}
     </h1>
-    <button @click="counter.increment()">Increase</button>
+    <button @click="counter.increment()"
+    :disabled="! counter.remaining"
+    >Increase ({{ counter.remaining }}) Remaining</button>
   </div>
 </template>
 
