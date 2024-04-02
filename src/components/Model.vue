4b63d95f302867e1dbe89d5a4969c8b65436e883
Kawesom <Kamaladeyinka@yahoo.com>
2024-04-03 10:26:52 +1100

    EP-29 Two Ways of Transitioning

--- src/components/Model.vue
+++ src/components/Model.vue
@@ -1,4 +1,5 @@
 <template>
+    <Transition name="fade">
     <div v-if="show" class="model-mask">
         <div class="model-container">
 
@@ -15,6 +16,7 @@
             </footer>
         </div>
     </div>
+    </Transition>
 </template>
 
 <script setup>
@@ -24,6 +26,18 @@ defineProps({
 </script>
 
 <style scoped>
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to, .fade-leave-from {
+    opacity: 100;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 1s
+}
+
 .model-mask {
     position: fixed;
     inset: 0;
@@ -57,3 +71,14 @@ defineProps({
     background: #c8c8c8;
 }
 </style>
+
+/* can be put in the transition <transition-group>
+
+</transition-group>
+enter-from-class="opacity-0 scale-150"
+enter-to-class="opacity-100 scale-100"
+enter-active-class="transition duration-300"
+leave-from-class="opacity-100 scale-100"
+leave-to-class="opacity-0 scale-150"
+leave-active-class="transition duration-200"
+*/
