b45b4572ad0b64ce668e5b27ed1badebb1f5a3aa
Kawesom <Kamaladeyinka@yahoo.com>
2024-04-03 11:26:18 +1100

    EP-30 Teleporting

---
+++ src/components/Teams/AddMemberModal.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Model from '@/components/Model.vue';
+import { ref } from 'vue';
+import { useTeamStore } from '@/stores/teamStore.js'
+
+
+let showModel = ref(false);
+let team = useTeamStore();
+
+
+</script>
+
+<template>
+    <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+    :disabled="! team.spotsRemaining"
+    @click="showModel = true"
+    >Add Member ({{ team.spotsRemaining }} Spots Left)</button>
+
+    <Teleport to="body">
+        <Model :show="showModel" @close="showModel = false">
+      
+          <template #default>
+            <p>Add a new member to your Team</p>
+      
+            <form class="mt-6">
+              <div class="flex gap-2">
+              <input type="email" placeholder="Email Address...." class="flex-1">
+              <button>Add</button>
+              </div>
+            </form>
+          </template>
+      
+        </Model>
+        </Teleport>
+</template>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TeamMembers from '@/components/Teams/TeamMembers.vue';
 import TeamHeader from '@/components/Teams/TeamHeader.vue';
-import Model from '@/components/Model.vue';
+
 import TeamFooter from '@/components/Teams/TeamFooter.vue';
 import { useTeamStore } from '@/stores/teamStore.js'
 import { ref } from 'vue';
@@ -13,11 +13,11 @@ setTimeout(() => {
   team.grow(25)
 }, 2000)
 
-let showModel = ref(false);
+
 </script>
 
 <template>
-  <TeamHeader @add="showModel = true" />
+  <TeamHeader />
 
   <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
     <TeamMembers />
@@ -25,18 +25,4 @@ let showModel = ref(false);
 
   <TeamFooter />
 
-  <Model :show="showModel" @close="showModel = false">
-
-    <template #default>
-      <p>Add a new member to your Team</p>
-
-      <form class="mt-6">
-        <div class="flex gap-2">
-        <input type="email" placeholder="Email Address...." class="flex-1">
-        <button>Add</button>
-        </div>
-      </form>
-    </template>
-
-  </Model>
 </template>
